28c53bde58337a7a4c70dd324cd64d28f8a3a0fa
rohitk0402 <rohitkrraj223344@gmail.com>
2025-08-25 09:57:41 +0530

    add url

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(fileUpload({
 
 app.use(cors(
   {
-    origin:process.env.Frontend_url,
+    origin:https://course-selling-app-2-6vt3.onrender.com ,
     credentials:true,
     methods:["GET","POST","PUT","DELETE"],
     allowedHeader:["Context-Type","Authorization"],
